4c65a873a812f858bc3e51a620a4d8f48493ce1f
Aya Ali <1aya.dn.ali@gmail.com>
2022-08-12 21:20:15 +0300

    finish

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (array, name) =>  {
+    return array.filter(
+      (element) => element.toLowerCase() === name.toLowerCase()
+    );
+  }
+//   console.log(findMatching(drivers,'Bobby'));
+
+// ************************************* 
+
+const fuzzyMatch = (arrays,names) => {
+        return arrays.filter(
+            (elements) => elements.toLowerCase().indexOf(names.toLowerCase()) === 0
+    );
+}
+console.log(fuzzyMatch (drivers,"koko"));
+// ********************************************
+
+const matchName = (arr,Name) =>{
+    return arr.filter((re) => re.name ===Name);
+}
